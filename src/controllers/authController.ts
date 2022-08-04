18bea1eea2c8c2cb19f8cc01f20669f63dbe2f7f
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-08-04 11:39:17 -0300

    refactor: changed controllers, repo and services

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express"
 
 import { authService } from "../services/authService.js"
 
-export async function signUp(req: Request, res: Response) {
-    await authService.userSignUp(req.body)
-    return res.sendStatus(201)
-}
+export const authController = {
+    async signUp(req: Request, res: Response) {
+        await authService.userSignUp(req.body)
+        return res.sendStatus(201)
+    },
 
-export async function signIn(req: Request, res: Response) {
-    const token = await authService.userSignIn(req.body)
-    return res.status(200).send({ token })
+    async signIn(req: Request, res: Response) {
+        const token = await authService.userSignIn(req.body)
+        return res.status(200).send({ token })
+    },
 }
-
-export const authController = { signUp, signIn }
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -9,17 +9,17 @@ import { createAndSendToken } from "../utils/createAndSendToken.js"
 export type userData = Omit<users, "id" | "createdAt" | "pictureURL">
 export type createUser = Omit<users, "id" | "createdAt">
 
-async function userSignUp(body: createUser) {
-    await ensureEmailIsNotInUse(body.email)
+export const authService = {
+    async userSignUp(body: createUser) {
+        await ensureEmailIsNotInUse(body.email)
 
-    await createAccount(body)
+        await createAccount(body)
+    },
+
+    async userSignIn(body: userData) {
+        const credentials = await checkEmail(body.email)
+        await validatePassword(credentials.password, body.password)
+
+        return await createAndSendToken(credentials.id, body.email)
+    },
 }
-
-async function userSignIn(body: userData) {
-    const credentials = await checkEmail(body.email)
-    await validatePassword(credentials.password, body.password)
-
-    return await createAndSendToken(credentials.id, body.email)
-}
-
-export const authService = { userSignUp, userSignIn }
