65d2a90d842e8a423e170463e8c45fe8cd3afd2c
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-08-05 22:44:19 -0300

    fix: get user stories query returning wrong ino

--- src/repositories/usersStoriesRepository.ts
+++ src/repositories/usersStoriesRepository.ts
@@ -2,9 +2,21 @@ import { client } from "../config/database.js"
 
 export const usersStoriesRepository = {
     async getUserStories(userId: number) {
-        const stories = await client.storiesUsers.findFirst({
+        const stories = await client.storiesUsers.findMany({
             where: { userId },
-            include: { story: true, user: true },
+            select: {
+                story: {
+                    select: {
+                        id: true,
+                        name: true,
+                        description: true,
+                        views: true,
+                        isFinished: true,
+                        bannerURL: true,
+                        language: { select: { name: true } },
+                    },
+                },
+            },
             orderBy: { story: { createdAt: "desc" } },
         })
         return stories
